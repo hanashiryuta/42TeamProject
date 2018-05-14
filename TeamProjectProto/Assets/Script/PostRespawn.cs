aba7f1a3b0028e913cd83057829779114c5f76bb
誠司 <masashi.k017a1376@gmail.com>
2018-05-14 16:54:37 +0900

    ポスト出現の間隔追加
    ・PostController.cs
    フィールドにbool型のactivityを追加
    Start()
    activityをtrueで追加
    Update()
    5ポイント貯まったとき、activityをfalseに変更
    最後にthis.gameobject.SetActiveを追加
    ・PostRespawn.cs
    フィールドにfloat型のintervalを追加
    Update()
    最後にポストが表示されていないときのif文を追加

--- TeamProjectProto/Assets/Script/PostRespawn.cs
+++ TeamProjectProto/Assets/Script/PostRespawn.cs
@@ -9,62 +9,71 @@ using UnityEngine;
 
 public class PostRespawn : MonoBehaviour {
 
-    List<GameObject> postList;//中心物体リスト
-    public GameObject originPost;//中心物体
-    public float postCount;//中心物体数
-    List<GameObject> childList;//移動先リスト
+	List<GameObject> postList;//中心物体リスト
+	public GameObject originPost;//中心物体
+	public float postCount;//中心物体数
+	List<GameObject> childList;//移動先リスト
+
+	float interval;//表示するまでの間隔
 
 	// Use this for initialization
 	void Start () {
-        //初期化処理
-        postList = new List<GameObject>();
-        childList = new List<GameObject>();
+		//初期化処理
+		postList = new List<GameObject>();
+		childList = new List<GameObject>();
 
-        //移動先リスト追加
-        for (int i = 0; i < transform.childCount; i++)
-        {
-            childList.Add(transform.GetChild(i).gameObject);
-        }
+		//移動先リスト追加
+		for (int i = 0; i < transform.childCount; i++)
+		{
+			childList.Add(transform.GetChild(i).gameObject);
+		}
 
-        //中心物体生成
-        for (int i = 0; i < postCount; i++) 
-        {
-            int rand;
-            
-            rand = Random.Range(0, childList.Count);
-            
-            //リスト追加
-            postList.Add(Instantiate(originPost, childList[rand].transform.position, Quaternion.identity, childList[rand].transform));
-            //移動先候補から現在の位置を除外
-            childList.RemoveAt(rand);
-        }
+		//中心物体生成
+		for (int i = 0; i < postCount; i++) 
+		{
+			int rand;
+
+			rand = Random.Range(0, childList.Count);
+
+			//リスト追加
+			postList.Add(Instantiate(originPost, childList[rand].transform.position, Quaternion.identity, childList[rand].transform));
+			//移動先候補から現在の位置を除外
+			childList.RemoveAt(rand);
+		}
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-        //中心物体リスト検索
-		foreach(var post in postList)
-        {
-            //移動可能なら
-            if (post.GetComponent<PostController>().isRespawn)
-            {
-                //現在のPosition保存
-                GameObject nowPosition = post.transform.parent.gameObject;
-              
-                int rand = Random.Range(0, childList.Count);
-                
-                //移動先Position保存
-                GameObject nextPosition = childList[rand];
-                
-                //今の移動先を次回の候補から除外して
-                childList.RemoveAt(rand);
-                //今のPositionを次回の候補に入れる
-                childList.Add(nowPosition);
+		//中心物体リスト検索
+		foreach (var post in postList) {
+			//移動可能なら
+			if (post.GetComponent<PostController> ().isRespawn) {
+				//現在のPosition保存
+				GameObject nowPosition = post.transform.parent.gameObject;
 
-                //移動処理
-                post.transform.position = nextPosition.transform.position;
-                post.GetComponent<PostController>().isRespawn = false;
-            }
-        }
+				int rand = Random.Range (0, childList.Count);
+
+				//移動先Position保存
+				GameObject nextPosition = childList [rand];
+
+				//今の移動先を次回の候補から除外して
+				childList.RemoveAt (rand);
+				//今のPositionを次回の候補に入れる
+				childList.Add (nowPosition);
+
+				//移動処理
+				post.transform.position = nextPosition.transform.position;
+				post.GetComponent<PostController> ().isRespawn = false;
+			}
+
+			//表示されていない場合の処理
+			if (post.GetComponent<PostController> ().activity==false) {
+				interval += 1;
+				if (interval >= 300) {
+					post.SetActive (post.GetComponent<PostController>().activity=true);
+					interval = 0;
+				}
+			}
+		}
 	}
 }
