e251d4db51f056a2d0d0b175386394fc78ef5c2e
TokisakiUrusi <38412514+TokisakiUrusi@users.noreply.github.com>
2018-05-25 10:50:03 +0900

    TitleSceneにロード画面を追加
    CanvasにLoadObj,FadePanelを追加
    EventSystemにGameLoad.csを追加
    ScriptにGameLoad.cs,FadeController.csを追加
    
    TitleScene.cs:
    ボタンを押したときにFadeController.csを呼び出しFadePanelを表示
    その後GameLoad.csを呼び出しロード画面を表示
    
    GameLoad.cs:
    LoadObjを表示し、一定時間バーを表示
    
    FadeController.cs:
    FadePanelのAlpha値を上昇させ、見えるようにする

---
+++ TeamProjectProto/Assets/FadeController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeController : MonoBehaviour {
+
+    [HideInInspector]
+    public Image image;
+    float tmpValue = 0;
+    public float seconds;
+
+	// Use this for initialization
+	void Start () {
+        image = this.GetComponent<Image>();
+        image.color = new Vector4(Color.black.r, Color.black.g, Color.black.b, 0);
+
+    }
+
+    public void ChangeAlpha() {
+        //image.color.a = alpha;
+
+        tmpValue += (255 / seconds) * Time.deltaTime * 60;
+
+        image.color = new Vector4(Color.black.r, Color.black.g, Color.black.b, tmpValue);
+    }
+
+    // Update is called once per frame
+    void Update () {
+	    
+	}
+}
