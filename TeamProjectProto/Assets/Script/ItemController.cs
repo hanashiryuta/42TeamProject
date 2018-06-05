2dae12cee24413668d47e37a0b2d3d643ba0139f
hanashiryuta <kbrn.kabirunrun@gmail.com>
2018-06-05 21:46:09 +0900

    Coin取得時のパーティクル追加
    ItemController.cs
    GameObject origin_Item_Death_Particle:新規作成
     アイテム取得時パーティクル生成元
    GameObject item_Death_Particle:新規作成
     アイテム取得時パーティクル
    Item_Death_Particle():新規作成
     アイテム取得時パーティクル生成メソッド
    
    PlayerHit.cs
    OnCollisionEnter()
     アイテム取得時パーティクル生成
    
    Material
    Star_M
     名前変更
    
    Prefab
    1CoinPointItem
     パーティクル追加
    2CoinPointItem
     パーティクル追加
    Coin_Death_Particle:新規作成
     アイテム取得時パーティクル
     Pig_ToCoin_Particle
     大きさ変更
    
    stage調整

--- TeamProjectProto/Assets/Script/ItemController.cs
+++ TeamProjectProto/Assets/Script/ItemController.cs
@@ -22,6 +22,9 @@ public class ItemController : MonoBehaviour {
     float moveTime = 0.5f;//吹き飛び時間
     Rigidbody rigid;//リジッドボディ
 
+    public GameObject origin_Item_Death_Particle;//アイテム取得時パーティクル生成元
+    GameObject item_Death_Particle;//アイテム取得時パーティクル
+
     // Use this for initialization
     void Start () {
         rigid = GetComponent<Rigidbody>();//リジッドボディ取得
@@ -31,35 +34,6 @@ public class ItemController : MonoBehaviour {
 	void Update () {
         if (!isGet)
         {
-            //positionX = moveX / 25;
-            //positionZ = moveZ / 25;
-
-            //foreach (var cx in Physics.OverlapBox(transform.position + new Vector3(positionX, 0, 0), new Vector3(transform.localScale.x / 2, transform.localScale.y / 2 - 0.05f, transform.localScale.z / 2), Quaternion.identity))
-            //{
-            //    //当たっているものが床だったら
-            //    if (cx.tag == "Field")
-            //    {
-            //        //移動しない
-            //        positionX = 0;
-            //        break;
-            //    }
-            //}
-
-            //foreach (var cx in Physics.OverlapBox(transform.position + new Vector3(0, 0, positionZ), new Vector3(transform.localScale.x / 2, transform.localScale.y / 2 - 0.05f, transform.localScale.z / 2), Quaternion.identity))
-            //{
-            //    //当たっているものが床だったら
-            //    if (cx.tag == "Field")
-            //    {
-            //        //移動しない
-            //        positionZ = 0;
-            //        break;
-            //    }
-            //}
-
-            
-
-            //transform.position += new Vector3(positionX, 0, positionZ);
-
             moveTime -= Time.deltaTime;
             if (moveTime <= 0)
             {
@@ -116,5 +90,16 @@ public class ItemController : MonoBehaviour {
         moveY = (float)Random.Range(0, 11) / 10;//Zランダム設定
 
         rigid.AddForce(300*new Vector3(moveX, moveY, moveZ).normalized);//移動処理
+    }
+
+    /// <summary>
+    /// アイテム取得時パーティクル生成メソッド
+    /// </summary>
+    public void Item_Death_Particle()
+    {
+        if(item_Death_Particle == null)
+        {
+            item_Death_Particle = Instantiate(origin_Item_Death_Particle, transform.position, Quaternion.identity);
+        }
     }
 }
