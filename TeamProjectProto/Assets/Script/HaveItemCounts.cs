843abc9b5e8c3f228b5823b79be6337b8eca3b2a
gowest2323 <35441974+gowest2323@users.noreply.github.com>
2018-06-21 16:38:07 +0900

    ルーレット中いないプレイヤーの手持ちを表示しない
    PlayerHaveItemCounts.prefabー＞格納オブジェリストがTextからGameObjectに
    
    main.unityー＞prefab変動
    
    HaveItemCounts.csー＞いないプレイヤーの手持ちを表示しない

--- TeamProjectProto/Assets/Script/HaveItemCounts.cs
+++ TeamProjectProto/Assets/Script/HaveItemCounts.cs
@@ -11,26 +11,35 @@ using DG.Tweening;
 
 public class HaveItemCounts : MonoBehaviour {
 
-    public List<Text> haveItemCounts;//表示テキストリスト
+    public List<GameObject> haveItemCounts;//表示テキストリスト
     RouletteController rouletteController;//ルーレット管理クラス
-    	
-	// Update is called once per frame
-	void Update () {
 
-        //ルーレットが入場状態なら
-        if (rouletteController.rouletteState == RouletteState.ENTRY)
+    bool isFirstTimeTextSeted = false;//最初のテキスト設定終わったか
+
+    void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update ()
+    {
+        if (isFirstTimeTextSeted)
         {
-            //出てくる
-            GetComponent<RectTransform>().DOLocalMoveY(200, 1);
-        }
-        //ルーレットが退場状態なら
-        else if(rouletteController.rouletteState == RouletteState.EXIT)
-        {
-            //引っ込む
-            GetComponent<RectTransform>().DOLocalMoveY(275, 1);
+            //ルーレットが入場状態なら
+            if (rouletteController.rouletteState == RouletteState.ENTRY)
+            {
+                //出てくる
+                GetComponent<RectTransform>().DOLocalMoveY(200, 1);
+            }
+            //ルーレットが退場状態なら
+            else if (rouletteController.rouletteState == RouletteState.EXIT)
+            {
+                //引っ込む
+                GetComponent<RectTransform>().DOLocalMoveY(275, 1);
+            }
         }
 
-	}
+    }
     /// <summary>
     /// 初期化クラス
     /// </summary>
@@ -42,10 +51,35 @@ public class HaveItemCounts : MonoBehaviour {
         transform.SetAsFirstSibling();
         //取得
         this.rouletteController = rouletteController;
+        //存在しているプレイヤーのインデックス格納
+        List<int> existPlayerIndexList = new List<int>();
         //テキスト設定
         for (int i = 0; i < pList.Length; i++)
         {
-            haveItemCounts[i].text = HalfWidth2FullWidth.Set2FullWidth(pList[i].GetComponent<PlayerMove>().holdItemCount);
+            //存在しているプレイヤーのインデックス取出し
+            int existPlayerIndex = (int)pList[i].GetComponent<PlayerMove>().playerIndex;
+            //格納
+            existPlayerIndexList.Add(existPlayerIndex);
+            //テキスト設定
+            haveItemCounts[existPlayerIndex].transform.
+                Find("Player" + (existPlayerIndex + 1) + "HaveCount").
+                GetComponent<Text>().text = HalfWidth2FullWidth.Set2FullWidth(pList[i].GetComponent<PlayerMove>().holdItemCount);
         }
+
+        int tmpIndex = 0;//一時用インデックス
+        //いないプレイヤー探し
+        foreach(var h in haveItemCounts)
+        {
+            //存在しているプレイヤーのインデックスリストになければ
+            if (!existPlayerIndexList.Contains(tmpIndex))
+            {
+                //非アクティブ化
+                h.SetActive(false);
+            }
+            tmpIndex++;
+        }
+
+        //最初のテキスト設定終わった
+        isFirstTimeTextSeted = true;
     }
 }
