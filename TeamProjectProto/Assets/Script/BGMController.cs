be40b0b17f44d371a8a90f7ef0522c9f49fa39fc
gowest2323 <35441974+gowest2323@users.noreply.github.com>
2018-06-22 09:52:06 +0900

    SE整理と追加
    新規
    SEController.cs
    素材多数

--- TeamProjectProto/Assets/Script/BGMController.cs
+++ TeamProjectProto/Assets/Script/BGMController.cs
@@ -1,4 +1,9 @@
-﻿using System.Collections;
+﻿/*
+ * 作成日時：180621
+ * BGMコントローラー
+ * 作成者：何承恩
+ */
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
---
+++ TeamProjectProto/Assets/Script/SEController.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * 作成日時：180622
+ * SEコントローラー
+ * 作成者：何承恩
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SEController : MonoBehaviour
+{
+    [SerializeField]
+    List<AudioClip> seList = new List<AudioClip>();//SE格納リスト
+    AudioSource audio;// AudioSource
+
+    // Use this for initialization
+    void Start ()
+    {
+        //このスクリプトを付けていつオブジェにAudioSourceコンポーネントを追加
+        transform.gameObject.AddComponent<AudioSource>();
+        //Audio設定
+        audio = transform.GetComponent<AudioSource>();
+        audio.playOnAwake = false;
+        audio.loop = false;//ループしない
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
